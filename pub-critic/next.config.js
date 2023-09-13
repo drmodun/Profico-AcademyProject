7e210bf8d356cbcca2d7763e15e7310bb06a1eb2
drmodun <jan.modun.st@gmail.com>
2023-09-13 13:30:20 +0200

    attempt at hotfix

--- pub-critic/next.config.js
+++ pub-critic/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost', 'pub-critic.netlify.app', "media.rawg.io", "gamesapiandcard-feature-3--pub-critic.netlify.app"],
+        domains: ["media.rawg.io"],
     },
 }
 
