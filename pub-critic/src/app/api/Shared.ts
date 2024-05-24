620306987e7205ec99a304754ece439daee9c3b4
drmodun <jan.modun.st@gmail.com>
2024-05-24 15:54:07 +0200

    change source api

--- pub-critic/src/app/api/Shared.ts
+++ pub-critic/src/app/api/Shared.ts
@@ -1,5 +1,5 @@
-export const baseURL = "https://agreeable-puce-suit.cyclic.app";
-export const altBaseURL = "https://pub-critic.fly.dev"; //if the other one is down
+//export const baseURL = "https://agreeable-puce-suit.cyclic.app";
+export const baseURL = "https://pub-critic.fly.dev"; //if the other one is down
 
 export const setJWT = (token: string) => {
   localStorage.setItem("jwtToken", token);
